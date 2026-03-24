02c758223e905fac7f9c75096e65314986c31c40
dinhsang2k5 <dinhsangusa@gmail.com>
2026-03-24 20:40:06 +0700

    fix conflict manually and add department module

---
+++ layout/repository/MssSQLConnection.cs
@@ -0,0 +1,32 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopShopApplication.Repository
+{
+    internal class MssSQLConnection
+    {
+        public SqlConnection dbConnection()
+        {
+            SqlConnection connection = null;
+            Console.WriteLine("Connecting to Sql Server.....");
+            try
+            {
+                // sửa lại tên phiên local database nếu xung đột
+                // : vd SQLEXPRESS thay cho SQLEXPRESS02
+                string connectionString = @"Server=.\SQLEXPRESS2022;Database=HumanResourceManagement;Integrated Security=true;TrustServerCertificate=true;";
+
+                return new SqlConnection(connectionString);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return connection;
+        }
+    }
+}
